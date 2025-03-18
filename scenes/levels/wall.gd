ff03054fdb35f2995dbfa68942c72fc38b42ec0d
Baptiste Reveyron <baptiste.reveyron@wakeo.co>
2025-03-18 09:48:52 +0100

    chore: wall are now box (csg doesn't work with quad mesh)

--- scenes/levels/wall.gd
+++ scenes/levels/wall.gd
@@ -28,8 +28,10 @@ func set_portal(portal: Portal) -> void:
 		return
 
 	_portals.push_back(portal)
-	await _update_mesh()
-	await _update_collision_shape()
+
+	await _compute()
+	#await get_tree().physics_frame
+	#await get_tree().physics_frame
 
 
 func unset_portal(portal: Portal) -> void:
@@ -37,22 +39,28 @@ func unset_portal(portal: Portal) -> void:
 		return
 
 	_portals.erase(portal)
-	await _update_mesh()
-	await _update_collision_shape()
 
+	await _compute()
+	#await get_tree().physics_frame
+	#await get_tree().physics_frame
 
 # Private
 
-func _compute_collision_shape() -> Shape3D:
+func _compute() -> void:
 	if _portals.is_empty():
+		print("empty")
 		var shape = BoxShape3D.new()
-		shape.size = Vector3(2, 2, 0.2)
-		return shape
+		shape.size = Vector3(2, 2, 0.05)
+		$CollisionShape.shape = shape
 
-	var base_mesh :=  BoxMesh.new()
-	base_mesh.size = Vector3(2, 2, 0.2)
+		var mesh :=  BoxMesh.new()
+		mesh.size = Vector3(2, 2, 0.05)
+		$MeshInstance.mesh = mesh
+		return
+
 	var csg_root := CSGMesh3D.new()
-	csg_root.mesh = 	base_mesh
+	csg_root.mesh = BoxMesh.new()
+	csg_root.mesh.size = Vector3(2, 2, 0.05)
 	add_child(csg_root)
 
 	# Use CSG tool to compute de the hole left by the opened portal in the mesh of the wall.
@@ -70,48 +78,10 @@ func _compute_collision_shape() -> Shape3D:
 	await get_tree().process_frame
 	await get_tree().process_frame
 
-	var computed_mesh = csg_root.get_meshes()[1]
-	remove_child(csg_root)
-	return computed_mesh.create_trimesh_shape()
-
-
-func _compute_meshes() -> Mesh:
-	var base_mesh :=  QuadMesh.new()
-	base_mesh.size = Vector2(2, 2)
-
-	if _portals.is_empty():
-		return base_mesh
-
-	var csg_root := CSGMesh3D.new()
-	csg_root.mesh = base_mesh
-	add_child(csg_root)
-
-	# Use CSG tool to compute de the hole left by the opened portal in the mesh of the wall
-	for portal in _portals:
-		var csg_child := CSGBox3D.new()
-		csg_root.add_child(csg_child)
-		csg_child.operation = CSGShape3D.OPERATION_SUBTRACTION
-		csg_child.quaternion = quaternion.inverse() * portal.quaternion
-		csg_child.global_position = portal.global_position
-		csg_child.size = Vector3(1, 1.9, 0.2)
-	csg_root.position = Vector3(1000, 1000, 1000)
-
-	# Wait for the CSG to compute the mesh
-	await get_tree().process_frame
-	await get_tree().process_frame
-
-	var meshes = csg_root.get_meshes()
-	var computed_mesh = meshes[1]
+	var mesh = csg_root.get_meshes()[1]
+	var shape = mesh.create_trimesh_shape()
 
 	remove_child(csg_root)
-	return computed_mesh
 
-
-func _update_collision_shape() -> void:
-	$CollisionShape.shape = await _compute_collision_shape()
-	await get_tree().physics_frame
-	await get_tree().physics_frame
-
-
-func _update_mesh() -> void:
-	$MeshInstance.mesh = await _compute_meshes()
+	$CollisionShape.shape = shape
+	$MeshInstance.mesh = mesh
